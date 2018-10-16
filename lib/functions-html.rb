8047dd2d916eb87df33cfb2067077a17f698bd92
Ben Sharpe <bsharpe@gmail.com>
2018-10-16 12:54:49 -0600

    rubocop cleanup

--- lib/functions-html.rb
+++ lib/functions-html.rb
@@ -1,16 +1,14 @@
-
-
 def html_action_form(action, inline = false, ap = nil, post = 'game.cgi')
   html = "<form action=\"#{post}\" method=\"post\" "
-  html += 'style="display:inline" ' if inline
-  html += '><input type="submit" value="' +
+  html << 'style="display:inline" ' if inline
+  html << '><input type="submit" value="' +
           action
-  html += ": #{ap}" unless ap.nil?
-  html += '" />' +
+  html << ": #{ap}" unless ap.nil?
+  html << '" />' +
           html_hidden('action', action.downcase) +
           html_hidden('magic', $user.magic)
-  html += yield if block_given?
-  html += '</form>'
+  html << yield if block_given?
+  html << '</form>'
   html
 end
 
@@ -34,18 +32,17 @@ end
 
 def html_drop_item(user)
   # generate the buttons that should be visible to user
-  tile = user.tile
-  user_id = user.mysql_id
   actions = user_actions(user)
   html = ''
   if actions.include?(:drop)
-    html += html_action_form('Drop') do
+    html << html_action_form('Drop') do
       html_select_num(15) +
         html_select_item(:plural) do |item|
           user.has_item?(item[:id])
         end
     end
   end
+  html
 end
 
 def html_forms(user)
@@ -65,7 +62,7 @@ def html_forms(user)
   html = ''
 
   if actions.include?(:attack) && has_targets
-    html += html_action_form('Attack') do
+    html << html_action_form('Attack') do
       html_select_target(animals + players + building) +
         ' with ' +
         html_select_item(:weapon, user.id) do |item|
@@ -75,7 +72,7 @@ def html_forms(user)
   end
 
   if actions.include?(:use)
-    html += html_action_form('Use') do
+    html << html_action_form('Use') do
       html_select_item do |item|
         !item[:use].nil? && item[:use] != :weapon && user.has_item?(item[:id])
       end +
@@ -86,14 +83,14 @@ def html_forms(user)
 
   buildables = build_list(user)
   if actions.include?(:build) && !buildables.empty?
-    html += html_action_form('Build') do
+    html << html_action_form('Build') do
       html_select_building(buildables)
     end
   end
 
   if actions.include?(:craft)
     craftables = craft_list(user.id)
-    html += html_action_form('Craft') do
+    html << html_action_form('Craft') do
       html_select_item(:craft) do |item|
         craftables.include? item
       end
@@ -101,7 +98,7 @@ def html_forms(user)
   end
 
   if actions.include?(:give) && has_targets
-    html += html_action_form('Give') do
+    html << html_action_form('Give') do
       html_select_num(15) +
         html_select_item(:plural) do |item|
           user.has_item?(item[:id])
@@ -112,7 +109,7 @@ def html_forms(user)
   end
 
   if actions.include?(:take)
-    html += html_action_form('Take') do
+    html << html_action_form('Take') do
       html_select_num(5) +
         html_select_item(:plural) do |item|
           stockpile_has_item?(user.x, user.y, item[:id])
@@ -122,7 +119,7 @@ def html_forms(user)
   end
 
   if actions.include?(:speak)
-    html += html_action_form('Say') do
+    html << html_action_form('Say') do
       html_text_box(200) + ' to ' +
         html_select_target(players) +
         '<br>Volume: ' +
@@ -132,33 +129,33 @@ def html_forms(user)
   end
 
   if actions.include?(:write)
-    html += html_action_form('Write', false, '3 ap') do
+    html << html_action_form('Write', false, '3 ap') do
       html_text_box(200) + ' on the building'
     end
 
   end
 
   if actions.include?(:sow)
-    html += html_action_form('Sow', false, '15 ap') do
+    html << html_action_form('Sow', false, '15 ap') do
       html_select_item(:plural) do |item|
         item[:plantable] == true
       end
     end
   end
-  html += html_action_form('Search', :inline) if actions.include?(:search)
-  html += html_action_form('Chop Tree', :inline, "#{chop_tree_ap(user.id)}ap") if actions.include?(:chop_tree) && (user.z.zero?)
-  html += html_action_form('Harvest', :inline, "#{harvest_ap(user.id)}ap") if actions.include?(:harvest)
-  html += html_action_form('Add Fuel', :inline) if actions.include?(:add_fuel)
-  html += html_action_form('Fill', :inline) if actions.include?(:fill)
-  html += html_action_form('Water', :inline) if actions.include?(:water)
-  html += html_action_form('Dig', :inline, '2 ap') if actions.include?(:dig) && (user.z.zero?)
-  html += html_action_form('Quarry', :inline, '4 ap') if actions.include?(:quarry)
+  html << html_action_form('Search', :inline) if actions.include?(:search)
+  html << html_action_form('Chop Tree', :inline, "#{chop_tree_ap(user.id)}ap") if actions.include?(:chop_tree) && user.z.zero?
+  html << html_action_form('Harvest', :inline, "#{harvest_ap(user.id)}ap") if actions.include?(:harvest)
+  html << html_action_form('Add Fuel', :inline) if actions.include?(:add_fuel)
+  html << html_action_form('Fill', :inline) if actions.include?(:fill)
+  html << html_action_form('Water', :inline) if actions.include?(:water)
+  html << html_action_form('Dig', :inline, '2 ap') if actions.include?(:dig) && user.z.zero?
+  html << html_action_form('Quarry', :inline, '4 ap') if actions.include?(:quarry)
   if actions.include?(:join)
-    html += html_action_form('Join Settlement', :inline, '25 ap') if user.settlement_id != tile.settlement_id
+    html << html_action_form('Join Settlement', :inline, '25 ap') if user.settlement_id != tile.settlement_id
   end
-  html += msg_no_ap(user.id) if actions.include?(:no_ap)
-  html += msg_no_ip if actions.include?(:no_ip)
-  html += html_action_form('Refresh', :inline)
+  html << msg_no_ap(user.id) if actions.include?(:no_ap)
+  html << msg_no_ip if actions.include?(:no_ip)
+  html << html_action_form('Refresh', :inline)
   html
 end
 
@@ -179,7 +176,7 @@ def html_inventory(user_id, y = nil, infix = ' x ', commas = false, inline = fal
              else
                user.item_count(item[:id])
              end
-    if amount.positive? 
+    if amount.positive?
       weight += (amount * item[:weight])
       item_desc = "<div title=\"#{item[:desc]}\""
       item_desc += ' style="display:inline"' if inline != false
@@ -195,10 +192,11 @@ def html_inventory(user_id, y = nil, infix = ' x ', commas = false, inline = fal
                  when (30..50) then 'Medium'
                  when (50..60) then 'Heavy'
                  when (60..Max_Weight) then 'Very Heavy'
-                 else 'You are over encumbered and cannot move'
-  end
+                 else
+                   'You are over encumbered and cannot move'
+                 end
 
-  html += if commas != false
+  html << if commas != false
             describe_list(item_descs)
           else
             item_descs.join
@@ -210,13 +208,14 @@ end
 def html_location_box(user)
   tile = Tile.new(user.x, user.y)
   html = '<div class="locationbox">'
-  html += tile.settlement.link if tile.settlement
+  html << tile.settlement.link if tile.settlement
   begin
-    html += '<br>' + lookup_table_row(:region, tile.region_id, :name)
-  rescue Exception => e
-    html += 'The Wilderness'
+    html << '<br>'
+    html << lookup_table_row(:region, tile.region_id, :name)
+  rescue StandardError
+    html << 'The Wilderness'
   end
-  html += "[#{user.x},#{user.y}]" + ' - ' + month + '</div>'
+  html << "[#{user.x},#{user.y}]" + ' - ' + month + '</div>'
   html
 end
 
@@ -229,13 +228,13 @@ def html_map(centre, size, user = nil, show_occupants = true, &block)
 
   html = '<table>'
   (-range..range).map do |y_offset|
-    html += '<tr>'
+    html << '<tr>'
     (-range..range).map do |x_offset|
-      html += html_tile((centre.x + x_offset), (centre.y + y_offset), z, user, show_buttons, show_occupants, &block)
+      html << html_tile((centre.x + x_offset), (centre.y + y_offset), z, user, show_buttons, show_occupants, &block)
     end
-    html += '</tr>'
+    html << '</tr>'
   end
-  html += '</table>'
+  html << '</table>'
   html
 end
 
@@ -278,9 +277,9 @@ def html_player_data(user)
              html + ", #{settlement.title} of "
            else html + ', pledged to '
            end
-    html += "<a href=\"settlement.cgi?id=#{settlement.mysql_id}\" class=\"ally\">#{settlement.name}</a>"
+    html << "<a href=\"settlement.cgi?id=#{settlement.mysql_id}\" class=\"ally\">#{settlement.name}</a>"
   end
-  html += '.<br>' \
+  html << '.<br>' \
          " HP: <b>#{user['hp']}/#{user['maxhp']}</b>" \
          " AP: <b>#{user['ap'].to_i.ceil}/#{Max_AP}</b>" \
          " <span style=\"font-size:80%\">(#{ap_recovery(user.id)} AP/hour)</span>" \
@@ -296,20 +295,20 @@ def html_select(coll, selected = nil)
   html = '<select name="option">'
   coll.each do |x|
     disp = block_given? ? yield(x) : x
-    html += "<option value=\"#{x}\""
-    html += ' selected="yes"' if selected == x
-    html += ">#{disp}</option>"
+    html << "<option value=\"#{x}\""
+    html << ' selected="yes"' if selected == x
+    html << ">#{disp}</option>"
   end
-  html += '</select>'
+  html << '</select>'
 end
 
 def html_select_building(buildings)
   html = '<select name="building" style="width:10em">'
   buildings.each do |building|
-    html += "<option value=\"#{building[:id]}\">"
-    html += "#{describe_craft(building)}</option>"
+    html << "<option value=\"#{building[:id]}\">"
+    html << "#{describe_craft(building)}</option>"
   end
-  html += '</select>'
+  html << '</select>'
 end
 
 def html_select_item(display = :name, user_id = nil)
@@ -319,9 +318,9 @@ def html_select_item(display = :name, user_id = nil)
   items = lookup_table(:item).values
   items = items.select { |item| yield(item) } if block_given?
   items.each do |item|
-    html += '<option '
-    html += 'selected="yes" ' if $params && $params['item'].to_i == item[:id]
-    html += 'value="' +
+    html << '<option '
+    html << 'selected="yes" ' if $params && $params['item'].to_i == item[:id]
+    html << 'value="' +
             item[:id].to_s + '">' +
             case display
             when :name then item[:name].capitalize
@@ -329,28 +328,28 @@ def html_select_item(display = :name, user_id = nil)
             when :craft then describe_craft(item)
             when :weapon then describe_weapon(item, user_id)
             end
-    html += '</option>'
+    html << '</option>'
   end
-  html += '</select>'
+  html << '</select>'
 end
 
 def html_select_num(number)
   html = '<select name="number">'
   (1..number).each do |n|
-    html += '<option value="' +
+    html << '<option value="' +
             n.to_s + '">' +
             n.to_s +
             '</option>'
   end
-  html += '</select>'
+  html << '</select>'
 end
 
 def html_select_target(targets, default = 'No-one', &block)
   html = '<select name="target" style="width:10em">'
-  html += "<option value=\"0:user\">#{default}</option>"
+  html << "<option value=\"0:user\">#{default}</option>"
 
   targets.each do |target|
-    html += case target.class.name
+    html << case target.class.name
             when 'Building'
               html_option_building(target)
             when 'Animal'
@@ -359,13 +358,13 @@ def html_select_target(targets, default = 'No-one', &block)
               html_option_user(target, &block)
             end
   end
-  html += '</select>'
+  html << '</select>'
 end
 
 def html_option_animal(animal)
   html = '<option '
-  html += 'selected="yes" ' if $target.mysql_id == animal.mysql_id
-  html += "value=\"#{animal.mysql_id}:animal\">" \
+  html << 'selected="yes" ' if $target.mysql_id == animal.mysql_id
+  html << "value=\"#{animal.mysql_id}:animal\">" \
           "#{animal.name} " \
           "(#{animal.hp}hp)" \
           '</option>'
@@ -375,16 +374,16 @@ def html_option_building(building)
   return '' unless building.exists?
 
   html = '<option '
-  html += 'selected="yes" ' if $target.is_a?(Building)
-  html += "value=\"#{building.x},#{building.y}:building\">#{building.name}</option>"
+  html << 'selected="yes" ' if $target.is_a?(Building)
+  html << "value=\"#{building.x},#{building.y}:building\">#{building.name}</option>"
 end
 
 def html_option_user(user)
   display = block_given? ? yield(user) : user.name
 
   html = '<option '
-  html += 'selected="yes" ' if !$target.nil? && $target.mysql_id == user.mysql_id
-  html += "value=\"#{user.mysql_id}:user\">#{display}</option>"
+  html << 'selected="yes" ' if !$target.nil? && $target.mysql_id == user.mysql_id
+  html << "value=\"#{user.mysql_id}:user\">#{display}</option>"
 end
 
 def html_skill(skill_name, user_id = 0, indent = 0, xp = 0, form = 'buy')
@@ -397,32 +396,32 @@ def html_skill(skill_name, user_id = 0, indent = 0, xp = 0, form = 'buy')
   style = user.has_skill?(skill[:id]) ? 'bought' : 'unbought'
 
   html = '<div style="padding:8px">'
-  indent.times { html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp' }
-  html += "<b class=\"#{style}\">" +
+  indent.times { html << '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp' }
+  html << "<b class=\"#{style}\">" +
           skill[:name].capitalize +
           "</b> - <i>#{skill[:desc]}</i>"
 
   if form == 'buy' && can_buy_skill?(user_id, skill[:id])
-    html += '<a onclick=\'javascript:return confirm("Learn skill ' + skill[:name] + '?")\' class=txlinkplain>'
-    html += html_action_form('Buy', true, "#{xp}xp", 'skills.cgi') do
+    html << '<a onclick=\'javascript:return confirm("Learn skill ' + skill[:name] + '?")\' class=txlinkplain>'
+    html << html_action_form('Buy', true, "#{xp}xp", 'skills.cgi') do
       html_hidden('skill', skill[:id])
     end
-    html += '</a>'
+    html << '</a>'
   end
 
   if can_sell_skill?(user_id, skill[:id])
-    html += '<a onclick=\'javascript:return confirm("Discard skill ' + skill[:name] + '?")\' class=txlinkplain>'
-    html += html_action_form('Sell', true, nil, 'skills.cgi') do
+    html << '<a onclick=\'javascript:return confirm("Discard skill ' + skill[:name] + '?")\' class=txlinkplain>'
+    html << html_action_form('Sell', true, nil, 'skills.cgi') do
       html_hidden('skill', skill[:id])
     end
-    html += '</a>'
+    html << '</a>'
   end
 
-  html += '</div>'
+  html << '</div>'
   subskills = lookup_all_where(:skill, :prereq, skill_name)
   unless subskills.empty?
     subskills.each do |sub|
-      html += html_skill(sub[:id], user_id, indent + 1, xp, form)
+      html << html_skill(sub[:id], user_id, indent + 1, xp, form)
     end
   end
   html
@@ -436,13 +435,13 @@ def html_skills_list(type, user_id = 0)
     user = User.new(user_id)
     level = user.level(type)
     xp_field = xp_field(type)
-    html += "<h2>Level #{level} #{lookup_table_row(:skills_renamed, :name, type).to_s.capitalize}</h2>" \
+    html << "<h2>Level #{level} #{lookup_table_row(:skills_renamed, :name, type).to_s.capitalize}</h2>" \
             "You have #{user.mysql[xp_field]} #{lookup_table_row(:skills_renamed, :name, type)} experience points.<br>"
   end
   form = user.level < Max_Level ? 'buy' : 'sell'
 
   skills.each do |skill|
-    html += html_skill(skill[:id], user_id, 0, skill_cost(level), form)
+    html << html_skill(skill[:id], user_id, 0, skill_cost(level), form)
   end
 
   html
@@ -471,12 +470,13 @@ def html_tile(x, y, z = 0, user = nil, button = false, occupants = true)
     button = tile_dir(user, tile)
     unless button.nil?
       ap_cost = ap_cost(tile.terrain, source_terrain, user.mysql_id, tile.settlement)
-      html += html_move_button(button, ap_cost)
+      html << html_move_button(button, ap_cost)
     end
   end
 
   if !tile.building_id.nil? && tile.building_id != 0
-    html << "<span class=\"mapdata\" style=\"color:#990000\">#{lookup_table_row(:building, tile.building_id, :name).capitalize}</span></br>"
+    html << "<span class=\"mapdata\" style=\"color:#990000\">" \
+      "#{lookup_table_row(:building, tile.building_id, :name).capitalize}</span></br>"
   end
 
   if occupants != true && occupants != :show_occupants
@@ -495,9 +495,9 @@ def html_tile(x, y, z = 0, user = nil, button = false, occupants = true)
   # show tile occupants if user is outside, or user is on tile
   if z.zero? || user.tile == tile
     users = mysql_count('users', { x: x, y: y, z: z, active: 1 }, id: user&.mysql_id)
-    if users.positive? 
+    if users.positive?
       html << "<span class=\"mapdata\">#{users} #{users == 1 ? 'person' : 'people'}</span>"
-     end
+    end
   end
   html << '</td>'
   html
